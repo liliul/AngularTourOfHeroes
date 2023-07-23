246e79d71291fe27f6c6f4beb3a8d2c73df9ffd0
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-07-23 15:24:36 -0300

    feat: usando rxjs update heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,10 +11,19 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
 
-  heroes = HEROES;
-
   selectedHero?: Hero;
     onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  constructor(private heroService: HeroService) {}
 }
